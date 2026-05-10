dd5129905e0706f032a620decbd8cbfe970dee40
VAnhNguyen2401 <anh.nv235651@sis.hust.edu.vn>
2026-05-10 21:32:33 +0700

    push API search

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,6 +2,8 @@ import express from 'express';
 import {
     getProfile,
     updateProfile,
+    getUserProfile,
+    searchUsers
 } from '../controllers/user.controller.js';
 
 import authMiddleware from '../middlewares/auth.middleware.js';
@@ -11,5 +13,9 @@ const router = express.Router();
 router.get('/profile', authMiddleware, getProfile);
 
 router.put('/profile', authMiddleware, updateProfile);
+
+router.get('/match', searchUsers);
+
+router.get('/:id', getUserProfile);
 
 export default router;
